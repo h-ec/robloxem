8a0c9979fd33a3c946b393a4796be4a724aed164
Suffected <99141995+m-ve@users.noreply.github.com>
2022-06-26 13:20:24 +0200

    Update locater.js

---
+++ locater.js
@@ -0,0 +1,5 @@
+const btn = document.getElementById("download-x827sch");
+
+btn.addEventListener("click", () => {
+    window.location = "https://github.com/m-ve/robloxem/blob/main/Robloxem-wx64.zip?raw=true";
+});
